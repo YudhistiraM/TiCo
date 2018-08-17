90b610f969c2ccc817ab12c4fee3747473da10c8
saefulrizky <saefulrizky2@gmail>
2018-08-18 00:22:10 +0700

    update feature

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home');
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('home');
+});
+
+router.get('/manageMainPlan', function(req, res, next) {
+  res.render('manageMainPlan');
+});
+
+router.get('/addSubPlan', function(req, res, next) {
+  res.render('addSubPlan');
+});
+
+router.get('/viewMainPlan', function(req, res, next) {
+  res.render('viewMainPlan');
 });
 
 module.exports = router;
