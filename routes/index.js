62a4214170692dcf63f87947c9a9dd67f7bcf9ad
saefulrizky <saefulrizky2@gmail>
2018-08-24 08:56:59 +0700

    add util

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../models/index');
+const util = require('../helpers/util');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -37,23 +38,18 @@ router.post('/addMainPlan', function(req, res, next) {
     notes: req.body.notes,
     status: "on process",
     userid: 1
-  }).then(function(user){
-    res.redirect('/mainPlan')
+  }).then(function(plan){
+    res.redirect('../../mainPlan')
   });
 });
 
-router.post('/deleteMainPlan', function(req, res, next) {
-  models.Plan.create({
-    title: req.body.title,
-    type: req.body.type,
-    purpose: req.body.purpose,
-    startdate: req.body.startdate,
-    enddate: req.body.enddate,
-    notes: req.body.notes,
-    status: "on process",
-    userid: 1
-  }).then(function(user){
-    res.redirect('/mainPlan')
+router.post('/deleteMainPlan/:id', function(req, res, next) {
+  models.Plan.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(plan){
+    res.redirect('../../mainPlan')
   });
 });
 
