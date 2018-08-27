04b2cd9bce5d007a147d3845bf8789a2249ba776
saefulrizky <saefulrizky2@gmail>
2018-08-27 07:51:31 +0700

    update helpers

--- helpers/util.js
+++ helpers/util.js
@@ -67,6 +67,15 @@ module.exports = {
       index--;
     }
     return fixedDays;
+  },
+
+  isLoggedIn: function(req, res, next){
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+    return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/login');
   }
 
 }
